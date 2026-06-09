a5f97a20054cb8342c617c6459b3b6bb02b6da26
idoceb00 <idoceb00@estudiantes.unileon.es>
2026-06-09 17:36:23 +0200

    Initial commit

---
+++ src/main/java/com/idoceb00/marketsimulator/MarketSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.idoceb00.marketsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSimulatorApplication.class, args);
+	}
+
+}
